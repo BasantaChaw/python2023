e18b16b786f38e5b5372343a1e284d66d672cc9e
Basanta Chaw <70615049+BasantaChaw@users.noreply.github.com>
2022-05-18 11:56:55 +0545

    Add files via upload

---
+++ Numpy/num1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 16 18:14:51 2022
+
+@author: Ibasanta Chaw
+"""
+
+import numpy as np
+a=np.array([1,2,3,4])
+print(a)
+
+#----------------------------------
+print('-----------------------------')
+b=np.array([[1,2,3],[2,5,3]])
+print(b)
